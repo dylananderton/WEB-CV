97615789f8e71c0942d33946c9b313f311363450
Dylan Anderton <66233295+dylananderton@users.noreply.github.com>
2022-04-09 17:05:54 +0800

    Add files via upload

--- cv.js
+++ cv.js
@@ -71,28 +71,148 @@ function topFunction() {
 	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
-$(document).ready(function() {
-	const $langhtml = $('.lang-html');
-	const $langc = $('.lang-c');
-	const $langcss = $('.lang-css');
-	$(window).resize(function(){
-	  if(($(window).width() > 767) && ($(window).width() < 992)){
-		$langhtml.html($langhtml.html().split('&nbsp;').join('<!--nbsp-->'));
-		$langhtml.html($langhtml.html().split('\u2002').join('<br>'));
-		$langhtml.html($langhtml.html().split('\u2003').join('<!--emsp-->'));
-		$langc.html($langc.html().split('\u2002').join('<br>'));
-		$langc.html($langc.html().split('\u2003').join('<!--emsp-->'));
-		$langcss.html($langcss.html().split('&nbsp;').join('<br>'));
-		$langcss.html($langcss.html().split('\u2003').join('<!--emsp-->'));
-	  }
-	  else {
-		$langhtml.html($langhtml.html().split('<!--nbsp-->').join('&nbsp;'));
-		$langhtml.html($langhtml.html().split('<br>').join('\u2002'));
-		$langhtml.html($langhtml.html().split('<!--emsp-->').join('\u2003'));
-		$langc.html($langc.html().split('<br>').join('\u2002'));
-		$langc.html($langc.html().split('<!--emsp-->').join('\u2003'));
-		$langcss.html($langcss.html().split('<br>').join('&nbsp;'));
-		$langcss.html($langcss.html().split('<!--emsp-->').join('\u2003'));
-	  }
-	})
-  });
+function skillsbr(){
+	var $containerWidth = $(window).width();
+	if($containerWidth < 992){
+		$("#skills-list #skillbr").removeClass("brmed");
+	}
+	else {
+		$("#skills-list #skillbr").addClass("brmed");
+	}
+}
+
+function skillslist(){
+	var $containerWidth = $(window).width();
+	if(($containerWidth > 767) && ($containerWidth < 992)){
+		$("#skills-list").hide();
+		$("#skills-list2").show();
+	}
+	else {
+		$("#skills-list").show();
+		$("#skills-list2").hide();
+	}
+}
+
+function hangman(){
+	var $containerWidth = $(window).width();
+	if($containerWidth < 768){
+		$("#hangman").hide();
+		$("#hangman2").show();
+	}
+	else {
+		$("#hangman2").hide();
+		$("#hangman").show();
+	}
+}
+
+$(document).ready(function () {
+    skillsbr();
+	skillslist();
+	hangman();
+});
+
+$(window).resize(function () {
+    skillsbr();
+	skillslist();
+	hangman();
+});
+// $(document).ready(function() {
+// 	$(window).resize(function(){
+// 	if($(window).width() < 992){
+// 		$("#skills-list #skillbr").removeClass("brmed");
+// 	}
+// 	else {
+// 		$("#skills-list #skillbr").addClass("brmed");
+// 	}
+// 	})
+// });
+
+// $(document).ready(function() {
+// 	$(window).resize(function(){
+// 	if(($(window).width() > 767) && ($(window).width() < 992)){
+// 		$("#skills-list").hide();
+// 		$("#skills-list2").show();
+// 	}
+// 	else {
+// 		$("#skills-list").show();
+// 		$("#skills-list2").hide();
+// 	}
+// 	})
+// });
+
+// $(document).ready(function() {
+// 	$(window).resize(function(){
+// 	if($(window).width() < 768){
+// 		$("#hangman").hide();
+// 		$("#hangman2").show();
+// 	}
+// 	else {
+// 		$("#hangman2").hide();
+// 		$("#hangman").show();
+// 	}
+// 	})
+// });
+
+// $(document).ready(function() {
+// 	const $skillslist = $('.skills-list');
+// 	$(window).resize(function(){
+// 	if($(window).width() < 992){
+// 		$("#skills-list #skillbr").removeClass("brmed");
+// 	}
+// 	else {
+// 		$("#skills-list #skillbr").addClass("brmed");
+// 	}
+// 	})
+// });
+
+// $(document).ready(function() {
+// 	$(window).resize(function(){
+// 	if(($(window).width() > 767) && ($(window).width() < 992)){
+// 		$("#skills-list").hide();
+// 		$("#skills-list2").show();
+// 	}
+// 	else {
+// 		$("#skills-list").show();
+// 		$("#skills-list2").hide();
+// 	}
+// 	})
+// });
+
+// $(document).ready(function() {
+// 	$(window).resize(function(){
+// 	if($(window).width() < 768){
+// 		$("#hangman").hide();
+// 		$("#hangman2").show();
+// 	}
+// 	else {
+// 		$("#hangman2").hide();
+// 		$("#hangman").show();
+// 	}
+// 	})
+// });
+
+// $(document).ready(function() {
+// 	const $langhtml = $('.lang-html');
+// 	const $langc = $('.lang-c');
+// 	const $langcss = $('.lang-css');
+// 	$(window).resize(function(){
+// 	  if(($(window).width() > 767) && ($(window).width() < 992)){
+// 		$langhtml.html($langhtml.html().split('&nbsp;').join('<!--nbsp-->'));
+// 		$langhtml.html($langhtml.html().split('\u2002').join('<br>'));
+// 		$langhtml.html($langhtml.html().split('\u2003').join('<!--emsp-->'));
+// 		$langc.html($langc.html().split('\u2002').join('<br>'));
+// 		$langc.html($langc.html().split('\u2003').join('<!--emsp-->'));
+// 		$langcss.html($langcss.html().split('&nbsp;').join('<br>'));
+// 		$langcss.html($langcss.html().split('\u2003').join('<!--emsp-->'));
+// 	  }
+// 	  else {
+// 		$langhtml.html($langhtml.html().split('<!--nbsp-->').join('&nbsp;'));
+// 		$langhtml.html($langhtml.html().split('<br>').join('\u2002'));
+// 		$langhtml.html($langhtml.html().split('<!--emsp-->').join('\u2003'));
+// 		$langc.html($langc.html().split('<br>').join('\u2002'));
+// 		$langc.html($langc.html().split('<!--emsp-->').join('\u2003'));
+// 		$langcss.html($langcss.html().split('<br>').join('&nbsp;'));
+// 		$langcss.html($langcss.html().split('<!--emsp-->').join('\u2003'));
+// 	  }
+// 	})
+//   });
